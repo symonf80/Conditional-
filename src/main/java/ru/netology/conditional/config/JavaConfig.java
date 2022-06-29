31057e23769be608b24b8edcde36749fd22250e2
symonf80 <symonf80@mail.ru>
2022-06-29 21:15:33 +0300

    add files

---
+++ src/main/java/ru/netology/conditional/config/JavaConfig.java
@@ -0,0 +1,23 @@
+package ru.netology.conditional.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import ru.netology.conditional.profile.DevProfile;
+import ru.netology.conditional.profile.ProductionProfile;
+import ru.netology.conditional.profile.SystemProfile;
+
+@Configuration
+public class JavaConfig {
+    @Bean
+    @ConditionalOnProperty(value = "netology.profile.dev", havingValue = "true")
+    public SystemProfile getProfile() {
+        return new DevProfile();
+    }
+
+    @Bean
+    @ConditionalOnProperty(value= "netology.profile.dev", havingValue = "false")
+    public SystemProfile prodProfile() {
+        return new ProductionProfile();
+    }
+}
